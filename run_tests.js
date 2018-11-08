5a5f0fb8a16517cfa1a685999e38f13fcac08c95
Sergey Zelenov <github@zelenov.su>
2018-11-08 21:40:29 +0100

    Initial setup: added run_tests.js

---
+++ run_tests.js
@@ -0,0 +1,31 @@
+const Mocha = require('mocha');
+const glob = require('glob');
+
+module.exports = function runTests(moduleName, taskName, options) {
+  if (!moduleName) {
+    console.error('Required parameter `moduleName` is missing. Example: `npm test 0-module 1-task`');
+    process.exit(1);
+  }
+  
+  if (!taskName) {
+    console.error('Required parameter `taskName` is missing. Example: `npm test 0-module 1-task`');
+    process.exit(1);
+  }
+
+  const tests = glob.sync(`${moduleName}/${taskName}/src/test/**/**.test.js`);
+  
+  if (tests.length === 0) {
+    console.error(`There are no test files in ${moduleName}/${taskName}. Please check your command.`);
+    process.exit(1);
+  }
+  
+  const mocha = new Mocha(options);
+  
+  tests.forEach(test => mocha.addFile(test));
+  
+  console.log('### RUN TESTS ###');
+  mocha.run(failures => {
+    console.log('### FINISH TESTS ###');
+    process.exitCode = (failures ? 1 : 0);
+  });
+};
